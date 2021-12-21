c5c16da45549dff291a5cab4ec1fb4c163d81752
benbaggley <ben.baggley@netsells.co.uk>
2021-12-21 15:19:47 +0000

    Fixed progress values

--- lib/src/bucket.dart
+++ lib/src/bucket.dart
@@ -152,7 +152,15 @@ class Bucket extends Client {
         headers['Authorization']!.split('Signature=').last;
     prevSignature = null;
 
-    http.StreamedRequest request = http.StreamedRequest('PUT', uri);
+    final request = _ProgressRequest(
+      'PUT',
+      uri,
+      onProgress: progressListener == null
+          ? null
+          : (bytes) {
+              progressListener(bytes.toDouble() / contentLength.toDouble());
+            },
+    );
     request.headers.addAll(headers);
 
     final futureRequest = request.send();
@@ -199,12 +207,7 @@ class Bucket extends Client {
 
       final completer = Completer();
       fileStream.listen((val) {
-        prevChunk = sendChunkRequestSync(val, prevChunk).whenComplete(() {
-          sent += val.length;
-          if (progressListener != null) {
-            progressListener(sent.toDouble() / contentLength.toDouble());
-          }
-        });
+        prevChunk = sendChunkRequestSync(val, prevChunk);
       }, onDone: () {
         sendChunkRequestSync([], prevChunk).then((etag) {
           completer.complete(etag);
@@ -280,5 +283,37 @@ class Bucket extends Client {
       throw ClientException(response.statusCode, response.reasonPhrase,
           response.headers.toSimpleMap(), body);
     }
+  }
+}
+
+class _ProgressRequest extends http.StreamedRequest {
+  /// Creates a new [_ProgressRequest].
+  _ProgressRequest(
+    String method,
+    Uri url, {
+    this.onProgress,
+  }) : super(method, url);
+
+  final void Function(int bytes)? onProgress;
+
+  /// Freezes all mutable fields and returns a
+  /// single-subscription [http.ByteStream]
+  /// that will emit the request body.
+  @override
+  http.ByteStream finalize() {
+    final byteStream = super.finalize();
+    if (onProgress == null) return byteStream;
+
+    var bytes = 0;
+
+    final t = StreamTransformer.fromHandlers(
+      handleData: (List<int> data, EventSink<List<int>> sink) {
+        bytes += data.length;
+        onProgress?.call(bytes);
+        sink.add(data);
+      },
+    );
+    final stream = byteStream.transform(t);
+    return http.ByteStream(stream);
   }
 }
